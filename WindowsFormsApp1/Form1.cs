83fa30963558853934a7705e934c54a5aaac5957
jrgs <jrgarcia@iesmarenostrum.com>
2022-12-02 17:29:33 +0100

    Versión inicial del proyecto

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtSaldo.Text = saldo.ToString();
+        }
+
+        private bool realizarReintegro(double cantidad) 
+        {
+            if (saldo > cantidad) {
+                saldo -= cantidad;
+                return true;
+            }
+            return false;
+        }
+
+        private void realizarIngreso(double cantidad) {
+            if (cantidad > 0)
+                saldo += cantidad;
+        }
+
+        private void btOperar_Click(object sender, EventArgs e)
+        {
+            double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
+            if (cantidad < 0) {
+                MessageBox.Show("Cantidad no válidá, sólo se admiten cantidades positivas.");
+            }
+            if (rbReintegro.Checked)
+            {
+                if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
+                    MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+            }
+            else
+                realizarIngreso(cantidad);
+            txtSaldo.Text = saldo.ToString();
+        }
+    }
+}
